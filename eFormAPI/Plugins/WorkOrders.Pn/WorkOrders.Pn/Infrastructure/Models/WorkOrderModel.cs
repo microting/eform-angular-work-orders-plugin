aa2c64c110c310d60dbb14f7b95e26246c2525f0
Leonid <leonid.atf@gmail.com>
2020-09-16 20:05:51 +0300

    Updates

--- eFormAPI/Plugins/WorkOrders.Pn/WorkOrders.Pn/Infrastructure/Models/WorkOrderModel.cs
+++ eFormAPI/Plugins/WorkOrders.Pn/WorkOrders.Pn/Infrastructure/Models/WorkOrderModel.cs
@@ -8,10 +8,12 @@ namespace WorkOrders.Pn.Infrastructure.Models
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public int CreatedByUserId { get; set; }
+        public string CreatedBy { get; set; }
         public string Description { get; set; }
         public DateTime CorrectedAtLatest { get; set; }
         public DateTime DoneAt { get; set; }
         public int DoneBySiteId { get; set; }
+        public string DoneBy { get; set; }
         public string DescriptionOfTaskDone { get; set; }
 
         public List<string> PicturesOfTask { get; set; } = new List<string>();
