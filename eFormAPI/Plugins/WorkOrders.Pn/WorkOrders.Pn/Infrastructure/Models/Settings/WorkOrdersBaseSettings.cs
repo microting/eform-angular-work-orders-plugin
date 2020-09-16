183e729a4086430c0c583d9b08ff099940074dc9
Leonid <leonid.atf@gmail.com>
2020-09-16 15:36:47 +0300

    Updates

--- eFormAPI/Plugins/WorkOrders.Pn/WorkOrders.Pn/Infrastructure/Models/Settings/WorkOrdersBaseSettings.cs
+++ eFormAPI/Plugins/WorkOrders.Pn/WorkOrders.Pn/Infrastructure/Models/Settings/WorkOrdersBaseSettings.cs
@@ -1,23 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WorkOrders.Pn.Infrastructure.Models.Settings
+﻿namespace WorkOrders.Pn.Infrastructure.Models.Settings
 {
     public class WorkOrdersBaseSettings
     {
         public string LogLevel { get; set; }
-
         public string LogLimit { get; set; }
-
         public string SdkConnectionString { get; set; }
-
         public string MaxParallelism { get; set; }
-
         public int NumberOfWorkers { get; set; }
-
-
         public int NewTaskId { get; set; }
         public int TaskListId { get; set; }
     }
