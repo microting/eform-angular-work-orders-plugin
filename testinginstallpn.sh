04ec8f0aff80b3e70e1f6f2a1c03869bb6aa7571
René Schultz Madsen <rm@microting.dk>
2020-09-16 17:05:47 +0000

    Fixing the testinginstall.sh

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="      .then(m => m.ItemsPlanningPnModule)\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="    loadChildren: () => import('\''./modules/items-planning-pn/items-planning-pn.module'\'')\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="    path: '\''items-planning-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="      .then(m => m.WorkOrdersPnModule)\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    loadChildren: () => import('\''./modules/workorders-pn/workorders-pn.module'\'')\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    path: '\''workorders-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  {\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
