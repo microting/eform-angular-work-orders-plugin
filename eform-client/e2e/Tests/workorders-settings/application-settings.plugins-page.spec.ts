9c651d66e04ce02ac7ed9586e53b88f1641af5bd
Gid <mkazli733@gmail.com>
2020-09-22 20:37:59 +0300

    Fixed path

--- eform-client/e2e/Tests/workorders-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/workorders-settings/application-settings.plugins-page.spec.ts
@@ -1,4 +1,4 @@
-import loginPage from '../../../Page objects/Login.page';
+import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
 import pluginPage from '../../Page objects/Plugin.page';
 
