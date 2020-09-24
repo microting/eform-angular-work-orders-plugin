5c70d5984ca8e768e159c2be9efa03182aaeb3c8
René Schultz Madsen <rm@microting.dk>
2020-09-24 18:43:13 +0000

    Fixing the code to not include none existing seeds.

--- eFormAPI/Plugins/WorkOrders.Pn/WorkOrders.Pn/Infrastructure/Models/Settings/WorkOrdersBaseSettings.cs
+++ eFormAPI/Plugins/WorkOrders.Pn/WorkOrders.Pn/Infrastructure/Models/Settings/WorkOrdersBaseSettings.cs
@@ -2,9 +2,6 @@
 {
     public class WorkOrdersBaseSettings
     {
-        public string LogLevel { get; set; }
-        public string LogLimit { get; set; }
-        public string SdkConnectionString { get; set; }
         public string MaxParallelism { get; set; }
         public int NumberOfWorkers { get; set; }
         public int NewTaskId { get; set; }
