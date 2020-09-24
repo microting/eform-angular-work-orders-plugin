f8fb0b50e1b2d8a79cc3f7f7828b698183020c40
Gid <mkazli733@gmail.com>
2020-09-24 14:57:59 +0300

    Refactor logic

--- eform-client/e2e/Page objects/WorkOrders/WorkOrders.page.ts
+++ eform-client/e2e/Page objects/WorkOrders/WorkOrders.page.ts
@@ -1,38 +1,82 @@
-import {PageWithNavbarPage} from '../PageWithNavbar.page';
+import { PageWithNavbarPage } from '../PageWithNavbar.page';
 import loginPage from '../Login.page';
 import myEformsPage from '../MyEforms.page';
-
 
 class WorkOrdersPage extends PageWithNavbarPage {
   constructor() {
     super();
   }
+
   public get rowNum(): number {
     return $$('#tableBody > tr').length;
   }
+
   public get firstWorkOrderId() {
     return $$('#workOrderId')[0].getText();
   }
+
   public goToWorkOrdersSettingsPage() {
     myEformsPage.Navbar.goToPluginsPage();
-    this.workOrderSettingsBtn.waitForDisplayed({timeout: 20000});
+    this.workOrderSettingsBtn.waitForDisplayed({ timeout: 20000 });
     this.workOrderSettingsBtn.click();
-    $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
+    $('#spinner-animation').waitForDisplayed({ timeout: 90000, reverse: true });
   }
+
+  public assignDeviceUser(name: string) {
+    $('#addNewSiteBtn').waitForDisplayed({ timeout: 20000 });
+    $('#addNewSiteBtn').click();
+    $('#selectDeviceUser').waitForDisplayed({ timeout: 20000 });
+    $('#selectDeviceUser').click();
+    $('#selectDeviceUser input').setValue(name);
+    $$('#selectDeviceUser .ng-option')[0].waitForDisplayed({ timeout: 20000 });
+    $$('#selectDeviceUser .ng-option')[0].click();
+    $('#siteAssignBtnSave').click();
+    $('#spinner-animation').waitForDisplayed({ timeout: 90000, reverse: true });
+  }
+
+  public removeAssignedDeviceUser() {
+    $('#addNewSiteBtn').waitForDisplayed({ timeout: 20000 });
+    $$('#removeSiteBtn')[$$('#removeSiteBtn').length - 1].click();
+    $('#removeSiteSaveBtn').waitForDisplayed({ timeout: 20000 });
+    $('#removeSiteSaveBtn').click();
+    $('#spinner-animation').waitForDisplayed({ timeout: 30000, reverse: true });
+  }
+
+  public cancelRemovingDeviceUser() {
+    $('#addNewSiteBtn').waitForDisplayed({ timeout: 20000 });
+    $$('#removeSiteBtn')[$$('#removeSiteBtn').length - 1].click();
+    $('#removeSiteSaveCancelBtn').waitForDisplayed({ timeout: 20000 });
+    $('#removeSiteSaveCancelBtn').click();
+  }
+
   public workOrdersBtn() {
-    $('#work-orders-pn').waitForDisplayed({timeout: 60000});
+    $('#work-orders-pn').waitForDisplayed({ timeout: 60000 });
     return $('#work-orders-pn');
   }
+
   public get workOrderSettingsBtn() {
     return $$('#plugin-settings-link')[$$('#plugin-settings-link').length - 1];
   }
+
   public goToOrdersPage() {
     this.workOrdersBtn().click();
     this.ordersBtn.click();
-    $('#spinner-animation').waitForDisplayed({timeout: 30000, reverse: true});
+    $('#spinner-animation').waitForDisplayed({ timeout: 30000, reverse: true });
   }
+
   public get ordersBtn() {
-    return $(`//*[contains(@class, 'fadeInDropdown')]//*[contains(text(), 'Orders')]`);
+    return $(
+      `/ /*[contains(@class, 'fadeInDropdown')]//*[contains(text(), 'Orders')]`
+    );
+  }
+
+  cancelAddingDeviceUser() {
+    $('#addNewSiteBtn').waitForDisplayed({timeout: 20000});
+    $('#addNewSiteBtn').click();
+    $('#selectDeviceUser').waitForDisplayed({timeout: 20000});
+    $('#selectDeviceUser').click();
+    $$('#selectDeviceUser .ng-option')[0].click();
+    $('#siteAssignBtnSaveCancel').click();
   }
 }
 
--- eform-client/e2e/Tests/workorders-general/workorders-settings.site.spec.ts
+++ eform-client/e2e/Tests/workorders-general/workorders-settings.site.spec.ts
@@ -9,59 +9,37 @@ const expect = require('chai').expect;
 const name = Guid.create().toString();
 const surname = Guid.create().toString();
 
-describe('Work Order Settings Site', function () {
+describe('Work Order Settings', function () {
   before(function () {
     loginPage.open('/');
     loginPage.login();
+
     myEformsPage.Navbar.goToDeviceUsersPage();
     $('#newDeviceUserBtn').waitForDisplayed({timeout: 20000});
-
     deviceUsersPage.createNewDeviceUser(name, surname);
   });
   it('Assign Site', function() {
     workOrdersPage.goToWorkOrdersSettingsPage();
-    $('#addNewSiteBtn').waitForDisplayed({timeout: 20000});
     const rowCountBeforeCreation = workOrdersPage.rowNum;
-    $('#addNewSiteBtn').click();
-    browser.pause(5000);
-    $('#selectDeviceUser').waitForDisplayed({timeout: 20000});
-    $('#selectDeviceUser').click();
-    browser.pause(1000);
-    $('#selectDeviceUser input').setValue(name);
-    browser.pause(5000);
-    $$('#selectDeviceUser .ng-option')[0].click();
-    $('#siteAssignBtnSave').click();
-    $('#spinner-animation').waitForDisplayed({timeout: 30000, reverse: true});
+    workOrdersPage.assignDeviceUser(name);
     const rowCountAfterCreation = workOrdersPage.rowNum;
     expect(rowCountAfterCreation, 'Number of rows hasn\'t changed after adding site').equal(rowCountBeforeCreation + 1);
   });
   it('Cancel Removing Site', function () {
-    $('#addNewSiteBtn').waitForDisplayed({timeout: 20000});
     const rowCountBefore = workOrdersPage.rowNum;
-    $$('#removeSiteBtn')[$$('#removeSiteBtn').length - 1].click();
-    $('#removeSiteSaveCancelBtn').waitForDisplayed({timeout: 20000});
-    $('#removeSiteSaveCancelBtn').click();
+    workOrdersPage.cancelRemovingDeviceUser();
     const rowCountAfter = workOrdersPage.rowNum;
     expect(rowCountAfter, 'Number of rows has changed').equal(rowCountBefore);
   });
   it('Remove Site', function () {
-    $('#addNewSiteBtn').waitForDisplayed({timeout: 20000});
     const rowCountBefore = workOrdersPage.rowNum;
-    $$('#removeSiteBtn')[$$('#removeSiteBtn').length - 1].click();
-    $('#removeSiteSaveBtn').waitForDisplayed({timeout: 20000});
-    $('#removeSiteSaveBtn').click();
-    $('#spinner-animation').waitForDisplayed({timeout: 30000, reverse: true});
+    workOrdersPage.removeAssignedDeviceUser();
     const rowCountAfter = workOrdersPage.rowNum;
     expect(rowCountAfter, 'Number of rows has changed').equal(rowCountBefore - 1);
   });
   it('Cancel Adding Site', function() {
-    $('#addNewSiteBtn').waitForDisplayed({timeout: 20000});
     const rowCountBefore = workOrdersPage.rowNum;
-    $('#addNewSiteBtn').click();
-    $('#selectDeviceUser').waitForDisplayed({timeout: 20000});
-    $('#selectDeviceUser').click();
-    $$('#selectDeviceUser .ng-option')[0].click();
-    $('#siteAssignBtnSaveCancel').click();
+    workOrdersPage.cancelAddingDeviceUser();
     const rowCountAfter = workOrdersPage.rowNum;
     expect(rowCountAfter, 'Number of rows has changed').equal(rowCountBefore);
   });
