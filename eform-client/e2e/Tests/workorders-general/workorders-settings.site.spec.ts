161721623c5bc687e88a3f986b2e3962cfcf0ab0
René Schultz Madsen <rm@microting.dk>
2020-11-10 15:17:44 +0000

    Adding navigation to / before checking.

--- eform-client/e2e/Tests/workorders-general/workorders-settings.site.spec.ts
+++ eform-client/e2e/Tests/workorders-general/workorders-settings.site.spec.ts
@@ -21,24 +21,32 @@ describe('Work Order Settings', function () {
   it('Assign Site', function() {
     const rowCountBeforeCreation = workOrdersPage.rowNum;
     workOrdersPage.assignDeviceUser(name);
+    loginPage.open('/');
+    workOrdersPage.goToWorkOrdersSettingsPage();
     const rowCountAfterCreation = workOrdersPage.rowNum;
     expect(rowCountAfterCreation, 'Number of rows hasn\'t changed after adding site').equal(rowCountBeforeCreation + 1);
   });
   it('Cancel Removing Site', function () {
     const rowCountBefore = workOrdersPage.rowNum;
     workOrdersPage.cancelRemovingDeviceUser();
+    loginPage.open('/');
+    workOrdersPage.goToWorkOrdersSettingsPage();
     const rowCountAfter = workOrdersPage.rowNum;
     expect(rowCountAfter, 'Number of rows has changed').equal(rowCountBefore);
   });
   it('Remove Site', function () {
     const rowCountBefore = workOrdersPage.rowNum;
     workOrdersPage.removeAssignedDeviceUser();
+    loginPage.open('/');
+    workOrdersPage.goToWorkOrdersSettingsPage();
     const rowCountAfter = workOrdersPage.rowNum;
     expect(rowCountAfter, 'Number of rows has changed').equal(rowCountBefore - 1);
   });
   it('Cancel Adding Site', function() {
     const rowCountBefore = workOrdersPage.rowNum;
     workOrdersPage.cancelAddingDeviceUser();
+    loginPage.open('/');
+    workOrdersPage.goToWorkOrdersSettingsPage();
     const rowCountAfter = workOrdersPage.rowNum;
     expect(rowCountAfter, 'Number of rows has changed').equal(rowCountBefore);
   });
