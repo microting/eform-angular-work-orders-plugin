54b0c403afb74f355c3cbf2a2c073ac388960084
René Schultz Madsen <rm@microting.dk>
2021-01-01 20:44:32 +0000

    Adding changes to make the test work better.

--- eform-client/e2e/Tests/workorders-general/workorders-settings.site.spec.ts
+++ eform-client/e2e/Tests/workorders-general/workorders-settings.site.spec.ts
@@ -16,6 +16,7 @@ describe('Work Order Settings', function () {
 
     deviceUsersPage.createDeviceUserFromScratch(name, surname);
 
+    $('#spinner-animation').waitForDisplayed({ timeout: 90000, reverse: true });
     workOrdersPage.goToWorkOrdersSettingsPage();
   });
   it('Assign Site', function() {
